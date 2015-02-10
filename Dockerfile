e1f462a8e8c70c30e80782e25a21a28a397e1594
Diego Alvarez Zuluaga <diego.alvarez.zuluaga@gmail.com>
2015-02-10 13:53:53 -0500

    Se adiciona la posibilidad de adicionar por microservicio una ip

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ MAINTAINER d1egoaz <diego.alvarez.zuluaga@gmail.com>
 COPY haproxy.cfg /usr/local/etc/haproxy/haproxy.cfg
 COPY localhost.suranet.com.pem /etc/ssl/private/localhost.suranet.com.pem
 EXPOSE 8080
-EXPOSE 80
 EXPOSE 443
 CMD ["haproxy", "-db", "-f", "/usr/local/etc/haproxy/haproxy.cfg"]
--- run.sh
+++ run.sh
@@ -1,5 +1,21 @@
+#!/bin/bash
+
+# Docker always give me the same ip, local server
+DOCKER_HOST_IP="172.17.42.1"
+
+# You could use a different ip x service
+IPWEB=$DOCKER_HOST_IP
+IPCONCEPTOS=$DOCKER_HOST_IP
+IPGENERADOR=$DOCKER_HOST_IP
+IPEJECUTOR=$DOCKER_HOST_IP
+IPHOMOLOGADOR=$DOCKER_HOST_IP
+
 docker run \
-  --add-host='dev.suranet.com:172.17.42.1' \
+  --add-host="dev-web.suranet.com:$IPWEB" \
+  --add-host="dev-conceptos.suranet.com:$IPCONCEPTOS" \
+  --add-host="dev-generador.suranet.com:$IPGENERADOR" \
+  --add-host="dev-ejecutor.suranet.com:$IPEJECUTOR" \
+  --add-host="dev-homologador.suranet.com:$IPHOMOLOGADOR" \
   -p 80:80 \
   -p 443:443 \
   -p 8080:8080 \
