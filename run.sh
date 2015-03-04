c8cdd7c373538327914a9dbdc6197888ce9e9f8c
yuliban <yulibanpenagos@seven4n.com>
2015-03-04 13:42:13 -0500

    Configuración integrador

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ IPCONCEPTOS=$DOCKER_HOST_IP
 IPGENERADOR=$DOCKER_HOST_IP
 IPEJECUTOR=$DOCKER_HOST_IP
 IPHOMOLOGADOR=$DOCKER_HOST_IP
+IPINTEGRADOR=$DOCKER_HOST_IP
 
 docker run \
   --add-host="dev-web.suranet.com:$IPWEB" \
@@ -16,6 +17,7 @@ docker run \
   --add-host="dev-generador.suranet.com:$IPGENERADOR" \
   --add-host="dev-ejecutor.suranet.com:$IPEJECUTOR" \
   --add-host="dev-homologador.suranet.com:$IPHOMOLOGADOR" \
+  --add-host="dev-integrador.suranet.com:$IPINTEGRADOR" \
   -p 80:80 \
   -p 443:443 \
   -p 8080:8080 \
